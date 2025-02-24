f09cd551d18b3b4ab0a301a3b7b32d44467aa703
Guilherme <guicieco@gmail.com>
2025-02-24 13:54:18 -0300

    Initial Adjustments

--- template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/UserConfiguration.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/UserConfiguration.cs
@@ -1,9 +1,11 @@
-﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+﻿#region Using
+
+using Ambev.DeveloperEvaluation.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System.Text.RegularExpressions;
-
 namespace Ambev.DeveloperEvaluation.ORM.Mapping;
+
+#endregion
 
 public class UserConfiguration : IEntityTypeConfiguration<User>
 {
@@ -18,6 +20,8 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
         builder.Property(u => u.Password).IsRequired().HasMaxLength(100);
         builder.Property(u => u.Email).IsRequired().HasMaxLength(100);
         builder.Property(u => u.Phone).HasMaxLength(20);
+        builder.Property(u => u.CreatedAt);
+        builder.Property(u => u.UpdatedAt);
 
         builder.Property(u => u.Status)
             .HasConversion<string>()
